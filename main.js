924590bde8a9b24c4ea9835763dc6be4ffcc3a6f
LukePatrickHasler <39388864+LHasler@users.noreply.github.com>
2018-07-26 13:27:59 -0400

    Update main.js
    Added JavaScript function for menu icon rotation

--- main.js
+++ main.js
@@ -1,12 +1,6 @@
-function myFunction() {
-    var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }
+/*main javascript*/ 
 
-  function myFunction(x) {
-    x.classList.toggle("change");
+
+function myFunction(x) {
+  x.classList.toggle("change");
 }
